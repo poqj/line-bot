8c3d197c0d62e65bf0b8d31c81030b6074cd2898
yang <lkk823354@gmail.com>
2025-09-19 00:27:10 +0800

    Successfully added GPT API into line bot

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+from openai import OpenAI
 
 from linebot.v3 import (
     WebhookHandler
@@ -23,6 +24,11 @@ app = Flask(__name__)
 configuration = Configuration(access_token='f6LAqltDbyrVnC6bdYUQTRD/vrbXyETeUSbZzGnUG7Tiy1viDVKLTG4g1tYlWBGvda704Z1WLsveWCRXEaVDoO2VSuAKYxqpIssxA0JbFZCnOAIRkNtnseolpl9jvFhf92oL/PXsA0+MqDbM6IbUywdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('46be525ddb8364e48c64667e65687cb9')
 
+with open('key.txt', 'r') as f:
+    api_key = f.read().strip()
+
+client = OpenAI(api_key=api_key)
+
 
 @app.route("/", methods=['POST'])
 def callback():
@@ -43,15 +49,29 @@ def callback():
     return 'OK'
 
 
+def use_gpt(user_msg): # use GPT API to generate a reply
+    completion = client.chat.completions.create(
+        model="gpt-5",
+        messages=[
+            {"role": "system", "content": "你是一個line聊天機器人."},
+            {"role": "user", "content": user_msg}
+        ]
+    )
+    gpt_reply = completion.choices[0].message.content
+    return gpt_reply
+
+
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     user_msg = event.message.text
-    reply_msg = '妳好'
+
+    gpt_reply = use_gpt(user_msg)
+
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         r = ReplyMessageRequest(
             reply_token=event.reply_token,
-            messages=[TextMessage(text=reply_msg)]
+            messages=[TextMessage(text=gpt_reply)]
             )
         line_bot_api.reply_message_with_http_info(r)
 
